0644a9918c15f22aa9f7398a3a30c77e1d5c0b15
fb0sh <fb0sh@outlook.com>
2025-03-23 16:45:01 +0800

    merge

--- learn_http/index.php
+++ learn_http/index.php
@@ -1,22 +1,17 @@
-
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $headers = getallheaders();
+    if (isset($headers['X-Query'])) {
+        $xQueryValue = $headers['X-Query'];
+        if ($xQueryValue == "flag") {
+            header('X-Res: flag{test_flag}');
+            echo "flag在哪呢?";
+        } else {
+            echo "你想要的不是flag吗？";
+        }
+    } else {
+        echo "请设置HTTP Header X-Query";
+    }
+} else {
     echo "请使用POST哦";
-    exit;
 }
-
-if (!isset($_SERVER['X-Query'])) {
-    echo "请设置HTTP Header X-Query";
-    exit;
-}
-
-$xQueryValue = $_SERVER['X-Query'];
-if ($xQueryValue !== "flag") {
-    echo "你想要的不是flag吗？";
-    exit;
-}
-
-header('X-Res: flag{test_flag}');
-
-echo "flag在哪里呢？";
